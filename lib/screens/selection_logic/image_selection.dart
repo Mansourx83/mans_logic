93a6464c408ec566a35974545917e26fb4edd9ab
Youssef Mansour <yousaf83000@gmail.com>
2025-11-20 16:51:02 +0200

    finish image selection

--- lib/screens/selection_logic/image_selection.dart
+++ lib/screens/selection_logic/image_selection.dart
@@ -8,8 +8,7 @@ class ImageSelection extends StatefulWidget {
 }
 
 class _ImageSelectionState extends State<ImageSelection> {
-
-  List<String> photos = [
+  final List<String> photos = [
     "assets/laptop/labtop1.png",
     "assets/laptop/labtop4.png",
     "assets/laptop/labtop2.png",
@@ -21,62 +20,92 @@ class _ImageSelectionState extends State<ImageSelection> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xcbfdee00),
+      backgroundColor: const Color(0xfff6f7f9),
 
       body: Column(
         children: [
+          const SizedBox(height: 80),
 
-          SizedBox(height: 100),
-          /// main Container
           AnimatedContainer(
-            height: 300,
-            duration: Duration(milliseconds: 400),
-            width: double.infinity,
-            child: Image.asset(photos[selectedIndex],fit: BoxFit.cover),
-          ),
+            duration: const Duration(milliseconds: 350),
+            curve: Curves.easeOut,
+            height: 400,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
 
-          SizedBox(height: 50),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: Colors.black.withOpacity(0.06),
+                width: 1.2,
+              ),
+            ),
 
-          /// other photos
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: List.generate(
-                  photos.length,
-                (index) {
-                   return InkWell(
-                     onTap: () {
-                       setState(() {
-                         selectedIndex = index;
-                       });
-                     },
-                     child: AnimatedContainer(
-                         duration: Duration(milliseconds: 400),
-                         width: 100,
-                         height: 100,
-                         margin: EdgeInsets.symmetric(horizontal: 3),
-                         decoration: BoxDecoration(
-                           color: Colors.white,
-                           borderRadius: BorderRadius.circular(4),
-                           border: Border.all(
-                             color:
-                             selectedIndex == index
-                             ? Colors.blue
-                             : Colors.white,
-                           ),
-                         ),
-                         child: Image.asset(photos[index],fit: BoxFit.cover),
-                     ),
-                   );
-                }
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 350),
+                transitionBuilder: (child, animation) {
+                  return FadeTransition(opacity: animation, child: child);
+                },
+                child: Image.asset(
+                  photos[selectedIndex],
+                  key: ValueKey(selectedIndex),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
 
+          const SizedBox(height: 35),
 
+          // Thumbnails
+          SizedBox(
+            height: 110,
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              scrollDirection: Axis.horizontal,
+              physics: BouncingScrollPhysics(),
+              itemCount: photos.length,
+              itemBuilder: (context, index) {
+                final isSelected = selectedIndex == index;
+
+                return GestureDetector(
+                  onTap: () => setState(() => selectedIndex = index),
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    margin: const EdgeInsets.symmetric(horizontal: 6),
+                    width: 95,
+                    height: 95,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: isSelected ? Colors.blue : Colors.transparent,
+                        width: 2.5,
+                      ),
+                      boxShadow: [
+                        if (isSelected)
+                          BoxShadow(
+                            color: Colors.black12.withOpacity(0.15),
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                      ],
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: AnimatedScale(
+                        scale: isSelected ? 1.05 : 1.0,
+                        duration: const Duration(milliseconds: 300),
+                        child: Image.asset(photos[index], fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
-
     );
   }
 }
